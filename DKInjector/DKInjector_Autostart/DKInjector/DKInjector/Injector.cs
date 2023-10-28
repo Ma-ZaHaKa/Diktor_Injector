aba0b80b68426e528309c82660ff2fb411afc172
Валодя Петров <96941376+Ma-ZaHaKa@users.noreply.github.com>
2023-10-28 19:21:07 +0300

    Add files via upload

---
+++ DKInjector/DKInjector_Autostart/DKInjector/DKInjector/Injector.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Injector
+{
+    public class Injector
+    {
+        // DLL imports
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        // Privileges 
+        private const int ProcessSuspendResume = 0x0800;
+        private const int ProcessCreateThread = 0x0002;
+        private const int ProcessQueryInformation = 0x0400;
+        private const int ProcessVmOperation = 0x0008;
+        private const int ProcessVmWrite = 0x0020;
+        private const int ProcessVmRead = 0x0010;
+        private const int ProcessAllAccess = ProcessCreateThread | ProcessQueryInformation | ProcessVmOperation | ProcessVmWrite | ProcessVmRead;
+
+        // Memory Allocation
+
+        private const uint MemoryCommit = 0x00001000;
+        private const uint MemoryReserve = 0x00002000;
+        private const uint PageReadWrite = 4;
+
+        //--GET--PID
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        public (bool, UInt32) GetGamePID(string proc_name)
+        {
+            UInt32 ret = 0;
+            Process[] proc = Process.GetProcessesByName(proc_name);
+
+            if (proc.Length == 0)
+            {
+                return (false, ret);
+            }
+
+            IntPtr hwGame = proc[0].MainWindowHandle;
+
+            if (hwGame == IntPtr.Zero)
+            {
+                return (false, ret);
+            }
+
+            GetWindowThreadProcessId(hwGame, out ret);
+
+            return (true, ret);
+        }
+
+        // @return List logs str-s
+        public List<string> Inject(string dllPath, string processName = "", int pid = 0)
+        {
+            // Get the process id
+            List<string> logs = new List<string>();
+            if (processName == "" && pid == 0) { logs.Add(@"Empty Proc Input"); return logs; }
+
+            var processId = 0;
+            if (processName != "") { processId = Process.GetProcessesByName(processName)[0].Id; }
+            else { processId = pid; }
+
+            // Get the address of LoadLibraryA
+
+            var loadLibraryA = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+            if (loadLibraryA == IntPtr.Zero)
+            {
+                var lastError = Marshal.GetLastWin32Error();
+                logs.Add(@"Failed to find kernel32.dll");
+            }
+
+            else
+            {
+                logs.Add(@"Successfully loaded kernel32.dll");
+            }
+
+            // Get the handle for the process
+
+            var processHandle = OpenProcess(ProcessAllAccess, false, processId);
+
+            if (processHandle == IntPtr.Zero)
+            {
+                var lastError = Marshal.GetLastWin32Error();
+                logs.Add(@"Failed to get process handle");
+            }
+
+            else
+            {
+                logs.Add(@"Successfully found process handle");
+            }
+
+            // Allocate memory in the process
+
+            var memory = VirtualAllocEx(processHandle, IntPtr.Zero, (uint)(dllPath.Length + 1), MemoryCommit | MemoryReserve, PageReadWrite);
+
+            if (memory == IntPtr.Zero)
+            {
+                var lastError = Marshal.GetLastWin32Error();
+                logs.Add(@"Failed to allocate memory");
+            }
+
+            else
+            {
+                logs.Add(@"Successfully allocated memory");
+            }
+
+
+            // Write memory in the process
+
+            if (WriteProcessMemory(processHandle, memory, Encoding.Default.GetBytes(dllPath), (uint)(dllPath.Length + 1), 0) == 0)
+            {
+                var lastError = Marshal.GetLastWin32Error();
+                logs.Add(@"Failed to write memory");
+            }
+
+            else
+            {
+                logs.Add(@"Successfully wrote memory");
+            }
+
+            // Create a thread to call LoadLibraryA in the process
+
+            if (CreateRemoteThread(processHandle, IntPtr.Zero, 0, loadLibraryA, memory, 0, IntPtr.Zero) == IntPtr.Zero)
+            {
+                var lastError = Marshal.GetLastWin32Error();
+                logs.Add(@"Failed to create remote thread");
+            }
+
+            else
+            {
+                logs.Add(@"Successfully created remote thread");
+            }
+
+            return logs;
+        }
+
+
+    }
+}
---
+++ DKInjector/DKInjector_Autostart/DKInjector/DKInjector/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.ConstrainedExecution;
+using System.Security;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Injector
+{ // BY Diktor
+    class Program
+    {
+        static async void ACmd(string line)
+        {
+            await Task.Run(() =>
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "cmd",
+                    Arguments = $"/c {line}",
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                });
+            });
+        }
+        static void Cmd(string line)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = $"/c {line}",
+                WindowStyle = ProcessWindowStyle.Hidden,
+            }).WaitForExit();
+        }
+
+        static async void AStartProc(string fpath)
+        {
+            /*await Task.Run(() =>
+            {
+                Process.Start(fpath);
+            });*/
+            //ACmd($"{fpath[0]}: & cd \"{Path.GetDirectoryName(fpath)}\" & start {Path.GetFileName(fpath)}");
+            ACmd($"cd \"{Path.GetDirectoryName(fpath)}\" & start {Path.GetFileName(fpath)}");
+            //ACmd($"cd \"{Path.GetDirectoryName(fpath)}\" & start revLoader.exe");
+        }
+        static bool IsProcessRunning(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            return processes.Length > 0;
+        }
+        static Process GetProcess(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            return processes.FirstOrDefault();
+        }
+
+        static void Main(string[] args)
+        {
+            // cfg
+            //proc:hl2.exe\r\ndll:C:\desk\r\ninj_delay:10
+            Injector inj = new Injector();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            string cfg = "cfg.ini";
+            //if (!File.Exists(cfg)) { File.WriteAllText(cfg, "proc:hl2.exe\r\ndll_paths:C:\\fullpath, C:\\fullpath2\r\ninj_delay_milis:2000\r\nproc_freeze:true"); Console.WriteLine("CFG MAKE!"); return; }
+            if (!File.Exists(cfg)) { return; }
+            string[] strs = File.ReadAllLines(cfg);
+
+            //----------------------------------
+            Console.ForegroundColor = ConsoleColor.Red;
+            if (strs.Length != 3) { Console.WriteLine("CFG ERROR!"); Console.ReadKey(); return; }
+
+            string path = strs[0].Replace("path:", "").Replace("\"", "").Replace("'", "");
+            if (!File.Exists(path)) { Console.WriteLine($"path \"{path}\" NOT EXISTS!"); return; }
+            List<string> dlls = strs[1].Replace("dll_paths:", "").Replace(", ", ",").Split(',').Select(x => x.Replace("\"", "").Replace("'", "")).ToList();
+            int delay = int.Parse(strs[2].Replace("inj_delay_milis:", ""));
+            //bool freeze = (strs[3].ToLower().Replace("proc_freeze:", "") == "true");
+
+            //----------------------------ERRRS
+            foreach (var dll in dlls) { if (!File.Exists(dll)) { Console.WriteLine($"DLL \"{dll}\" NOT FOUND!"); return; } }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine($"Waiting proc \"{proc}\"...");
+            //(bool, UInt32) p_id = (false, 0);
+            //while (true) { p_id = inj.GetGamePID(proc); if (p_id.Item1) { break; } else { System.Threading.Thread.Sleep(10); } }
+            ////if (!p_id.Item1) { Console.WriteLine($"PROC \"{proc}\" NOT FOUND!"); return; }
+            //---------------------------------
+
+
+            //---------START--PROC
+            /*ProcessStartInfo startInfo = new ProcessStartInfo(path);
+            startInfo.UseShellExecute = false;
+            Process proc = Process.Start(startInfo);*/
+            AStartProc(path);
+            string proc_name = Path.GetFileNameWithoutExtension(path);
+            while (true) { var p_id = inj.GetGamePID(proc_name); if (p_id.Item1) { break; } else { System.Threading.Thread.Sleep(10); } }
+            Process proc = GetProcess(proc_name);
+
+            //Console.WriteLine("Find! Waiting delay..");
+            System.Threading.Thread.Sleep(delay);
+            //inj.Inject(dll, pid: (int)p_id.Item2);
+
+            //if (freeze) { } // freeze proc
+            foreach (var dll in dlls)
+            {
+                inj.Inject(dll, proc.ProcessName);
+                System.Threading.Thread.Sleep(1 * 1000);
+                Console.WriteLine($"Injected \"{dll}\"");
+            }
+            //if (freeze) { } // unfreeze proc
+            //Console.WriteLine("OK!");
+
+            Console.WriteLine("DLL injected successfully.");
+        }
+    }
+}
